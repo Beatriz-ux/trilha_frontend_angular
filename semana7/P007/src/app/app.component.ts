13933db056505410b672d115bfdd5dabb84805a2
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-15 14:57:12 -0300

    feat: função que retorna as categorias

--- semana7/P007/src/app/app.component.ts
+++ semana7/P007/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'P007';
@@ -13,4 +13,11 @@ export class AppComponent {
     this.dados = event;
   }
 
+  getCategorias(): string[] {
+    return Object.keys(this.dados);
+  }
+
+  onBuscaCategoriaRealizada(): void {
+    
+  }
 }
