6619708b3201cb46afca0f4e28504afc6e805ae6
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-15 19:18:05 -0300

    feat: exibindo propriedade

---
+++ semana7/P007/src/app/valor-propriedade/valor-propriedade.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-valor-propriedade',
+  templateUrl: './valor-propriedade.component.html',
+  styleUrl: './valor-propriedade.component.css'
+})
+export class ValorPropriedadeComponent {
+  @Input()
+  value = "";
+  
+}
