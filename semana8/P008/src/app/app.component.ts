c80db8a97ee464eab6c54daf1c829f8b7c5a7f0c
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-01-25 07:40:43 -0300

    feat: Atividade busca WikiApi

--- semana8/P008/src/app/app.component.ts
+++ semana8/P008/src/app/app.component.ts
@@ -8,5 +8,34 @@ import { ListaNewsService } from './service/lista-news.service';
 })
 export class AppComponent {
   title = 'P008';
+  lista_news: INews[] = [];
   constructor(private service : ListaNewsService) {}
+  onBuscaRealizada(termo: string){
+    this.service.buscaAPI(termo)
+    .subscribe(
+      (data) => {
+
+      this.lista_news = data as INews[];
+      console.log(data);
+    }
+    );
+    
+  }
+}
+interface INews {
+  batchcomplete:string;
+  continue:{};
+  query: IQuery;
+}
+interface IQuery {
+  searchinfo:{};
+  search: ISearch[];
+}
+interface ISearch {
+  ns:number;
+  title:string;
+  pageid:number;
+  size:number;
+  wordcount:number;
+  snippet:string;
 }
--- semana8/P008/src/app/app.module.ts
+++ semana8/P008/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BuscaComponent } from './busca/busca.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { BuscaComponent } from './busca/busca.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
