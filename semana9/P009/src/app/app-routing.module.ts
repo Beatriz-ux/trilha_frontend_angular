7e34e7473bf304c8ce95962562725e5853c24acb
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-02-02 23:32:25 -0300

    Add menu styles and project component

--- semana9/P009/src/app/app-routing.module.ts
+++ semana9/P009/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Projeto1Component } from './projeto1/projeto1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'projeto1' , component: Projeto1Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- semana9/P009/src/app/app.module.ts
+++ semana9/P009/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Projeto1Component } from './projeto1/projeto1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Projeto1Component
   ],
   imports: [
     BrowserModule,
