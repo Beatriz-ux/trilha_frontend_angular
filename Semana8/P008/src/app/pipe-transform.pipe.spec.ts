8efa6f4ef560fb361fcdc20f8fdfec32f0962df5
Beatriz <82838311+Beatriz-ux@users.noreply.github.com>
2024-02-03 01:41:29 -0300

    Delete Semana8 directory

--- Semana8/P008/src/app/pipe-transform.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PipeTransformPipe } from './pipe-transform.pipe';
-
-describe('PipeTransformPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PipeTransformPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
