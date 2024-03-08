b104837cb1a7a702e9bc74632abfb32bf61a42d9
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-03-07 23:25:29 -0300

    Add new images and update styles.css and cadastro.component.ts

--- semana15/P015/src/app/cadastro/cadastro.component.ts
+++ semana15/P015/src/app/cadastro/cadastro.component.ts
@@ -4,27 +4,51 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrl: './cadastro.component.css',
 })
-export class CadastroComponent implements OnInit{
-  formsCadastro : FormGroup;
-  constructor() { 
+export class CadastroComponent implements OnInit {
+  formsCadastro: FormGroup;
+  bufferPassWord: string = '';
+  constructor() {
     this.formsCadastro = new FormGroup({
-      name: new FormControl(null, [Validators.required, Validators.maxLength(12), Validators.pattern(/^\S*$/)]),
-      email: new FormControl(null, [Validators.required, Validators.maxLength(6), Validators.email]),
-      passWord: new FormControl(null, [Validators.required, Validators.maxLength(4)]),
-      confirmPassword: new FormControl(null),
-      nameFarm: new FormControl(null, [Validators.required, Validators.maxLength(5)]),
-  });
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^\p{L}+(\s+\p{L}+)+$/u),
+      ]),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.email,
+      ]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl(null, [Validators.required]),
+      nameFarm: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  cadastrar(){
+  cadastrar() {
     console.log('Cadastrando...');
     console.log(this.formsCadastro);
   }
+  comparePassword(pass: any): void {
+    console.log(pass.target.value);
+    console.log(this.bufferPassWord);
+    const confirmPasswordControl =
+      this.formsCadastro.controls['confirmPassword'];
 
-
+    if (pass.target.value === this.bufferPassWord) {
+      console.log('Senhas iguais');
+      confirmPasswordControl.setErrors(null);
+    } else {
+      confirmPasswordControl.setErrors({ notSame: true });
+    }
+  }
+  bufferPass(pass: any) {
+    this.bufferPassWord = pass.target.value;
+  }
 }
